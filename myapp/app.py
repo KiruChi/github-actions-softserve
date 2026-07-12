ca49b9050bd73fee287667bee31a06f61d3534f0
KiruChi <luchaninovarseniy@gmail.com>
2026-07-12 22:31:37 +0300

    added failure

--- myapp/app.py
+++ myapp/app.py
@@ -2,6 +2,7 @@ def add(a, b):
     """Функція додавання"""
     return a + b
 
-# def subtract(a, b):
-#     """Функція віднімання"""
-#     return a - b
+def subtract(a, b):
+    # """Функція віднімання"""
+    # return a - b
+    pass
